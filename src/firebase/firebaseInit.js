d732a4cdb3d4f7ca591b7fd2f37f19b0d6370d76
Ahmed Nabil <a7med.nabil1655@gmail.com>
2022-10-28 09:32:00 +0200

    Firebase and Firestore Setup

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtp7OYbdsHA5FEGKCJ5jqjY7xvKv7DFVo",
+  authDomain: "vue-invoice-app-62f1e.firebaseapp.com",
+  projectId: "vue-invoice-app-62f1e",
+  storageBucket: "vue-invoice-app-62f1e.appspot.com",
+  messagingSenderId: "99719160950",
+  appId: "1:99719160950:web:ba4c2ab91304278e68ab57",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
